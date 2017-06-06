debe26eb930d57c66cb0c138b46c5a39e3d38434
xiaojieluo <xiaojieluoff@gmail.com>
2017-06-06 16:40:05 +0800

    update

--- pelican_autotool.py
+++ pelican_autotool.py
@@ -8,7 +8,8 @@ import yaml
 
 PATH = 'content'
 type_ = 'resTructured' # Markdown
-FILENAME = '%Y-%m-%d-{title}'
+# FILENAME = '%Y-%m-%d-{title}'
+FILENAME = '{time}-{title}'
 AUTHOR = 'xiaojieluo'
 
 class Autotool(object):
@@ -26,10 +27,20 @@ class Autotool(object):
     def __init__(self, argv):
         self.argv = argv
         self.data = {
-            'create':'','author':'','path':self.get_path(),'type':'','author':'','filename':'%Y-%m-%d-{title}'
+            'title':'',
+            'date':time.strftime('%Y-%m-%d', time.localtime()),
+            'time':time.strftime('%H:%M:%S', time.localtime()),
+            'author':'',
+            'type':'rst',
+            'summary':'',
+            'author':'xiaojieluo',
+            'tags':'',
+            'slug':'',
+            'filename':'{date}-{title}',
+            'path':self.get_path(),
         }
-        # if self.data.get('path') == '':
-        #     self.data['path'] = self.get_path()
+        self.content = list()
+
         self.data.update(self._get_conf())
 
     def _get_conf(self):
@@ -60,15 +71,24 @@ class Autotool(object):
         if 'help' in self.argv:
             self.show_help() # 显示帮助信息之后退出程序
             return
-        if '-a' in self.argv:
-            self._get_para(self.argv.index('-a'), 'author')
-        if '-p' in self.argv:
-            self._get_para(self.argv.index('-p'), 'path')
-        if '-t' in self.argv:
-            self._get_para(self.argv.index('-t'), 'type')
+        if '-author' in self.argv:
+            self._get_para(self.argv.index('-author'), 'author')
+        if '-date' in self.argv:
+            self._get_para(self.argv.index('-date'), 'date')
+        if '-path' in self.argv:
+            self._get_para(self.argv.index('-path'), 'path')
+        if '-type' in self.argv:
+            self._get_para(self.argv.index('-type'), 'type')
+        if '-time' in self.argv:
+            self._get_para(self.argv.index('-time'), 'time')
+        if '-tags' in self.argv:
+            self._get_para(self.argv.index('-tags'), 'tags')
+
         if 'create' in self.argv:
-            self._get_para(self.argv.index('create'), 'create')
-            self.create(self.argv)
+            self._get_para(self.argv.index('create'), 'title')
+            if len(self.argv) > self.argv.index('create') +1:
+                self.create(self.argv)
+
             return
 
     def _get_para(self, index, name):
@@ -78,38 +98,51 @@ class Autotool(object):
         if len(self.argv) > index+1:
             self.data[name] = self.argv[index+1]
         else:
-            # self.data[name] = ''
             print("参数不完整")
-            # return None
 
     def create(self,argv):
         '''
         创建指定的文章
         '''
-        if self.data.get('create'):
-            filename = (time.strftime(FILENAME, time.localtime())).format(title=self.data.get('create'))
-            content = self.data.get('create') + '\n' + (len(self.data.get('create'))+2)*'=' + \
-                    '\n:date: '+ time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + \
-                    '\n:slug: ' + replace(self.translation(self.data.get('create'))) + \
-                    '\n:modified: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + \
-                    '\n:author: ' + self.data.get('author')
+
+        if self.data.get('title'):
+            # if self.data.get('date') == '':
+            #     self.data['date'] = time.strftime('%Y-%m-%d', time.localtime());
+
+            filename = FILENAME.format(time=self.data['date'], title=self.data['title'])
+
+
+            self.content.append(self.data['title'])
+            self.content.append(len(self.data['title'])*'==' + '\n')
+            self.content.append(':date: ' + self.data['date'] + ' ' + self.data['time'])
+            self.content.append(':modified: ' + self.data['date'] + ' ' + self.data['time'])
+            self.content.append(':slug: ' + self.replace(self.translation(self.data['title'])))
+            if self.data.get('tags'):
+                self.content.append(':tags: ' + self.data['tags'])
+            self.content.append(':author: '+ self.data['author'])
+            self.content.append('\n')
 
             ext = '.rst' if (self.data.get('type') == ('rst' or 'resTructured'))  else '.md'
             fullname = os.path.join(self.data.get('path'), filename)+ext
-            
-            # 目录不存在,则创建
-            if os.path.isdir(os.path.join('./',self.data.get('path'))) is False :
-                print("目录不存在,创建中...........创建完成")
-                os.makedirs(os.path.join(self.data.get('path')))
 
-            with open(fullname, 'w') as fp:
-                fp.write(content)
-
-            print("{fullname} craete ....... [successful!]".format(fullname=fullname))
-            return
+            if self._create_file(fullname, '\n'.join(self.content)):
+                print("{fullname} craete ....... [successful!]".format(fullname=fullname))
+            else:
+                print("failed!")
         else:
             print("参数不完整")
             return
+
+    def _create_file(self, fullname, content):
+        # 目录不存在,则创建
+        if os.path.isdir(os.path.join('./',self.data.get('path'))) is False :
+            print("目录不存在,创建中...........创建完成")
+            os.makedirs(os.path.join(self.data.get('path')))
+
+        with open(fullname, 'w') as fp:
+            fp.write(content)
+
+        return True
 
     def get_path(self):
         '''
@@ -128,7 +161,7 @@ class Autotool(object):
         else:
             return ''
 
-    def translation(self, query, from_ = 'auto', to = 'en'):
+    def translation(self, query, from_ = 'zh', to = 'en'):
         '''
         请求百度翻译 api 获取翻译结果
         '''
@@ -144,6 +177,7 @@ class Autotool(object):
         r = requests.get(url)
         r.encoding = 'utf-8'
         result = r.json()
+        print(result)
         if 'error_code' in result:
             # raise TransLationError
             return None
@@ -153,70 +187,76 @@ class Autotool(object):
 
         return None
 
-def main():
-    argv = sys.argv
 
-    if len(argv) > 1:
-        make(argv)
-
-def make(argv):
-    if len(argv) > 1:
-        if argv[1] == 'create':
-            return create(argv)
-        else: return None
-
-def create(argv):
-    '''
-    创建指定的文章
-    '''
-    if len(argv) > 2:
-        filename = (time.strftime(FILENAME, time.localtime())).format(title=argv[2])
-        content = argv[2] + '\n' + (len(argv[2])+2)*'=' + \
-                '\n:date: '+ time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + \
-                '\n:slug: ' + replace(translation(argv[2])) + \
-                '\n:modified: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + \
-                '\n:author: ' + AUTHOR
-
-        ext = '.rst' if type_ == 'resTructured' else '.md'
-        with open(PATH+'/'+filename+ext, 'w') as fp:
-            fp.write(content)
-
-def replace(string):
-    '''
-    将空格转换成 -
-    '''
-    return string.replace(' ', '-')
-
-def translation(query, from_ = 'auto', to = 'en'):
-    '''
-    请求百度翻译 api 获取翻译结果
-    '''
-    appid = '20161217000034172'
-    secretKey = '07Qh2zEwKIx3kGwer1Uz'
-    salt = random.randint(32768, 65536)
-    sign = appid+query+str(salt)+secretKey
-    m1 = hashlib.md5(sign.encode('utf8'))
-    sign = m1.hexdigest()
-    url ='http://api.fanyi.baidu.com/api/trans/vip/translate?'
-    url = url + urllib.parse.urlencode({'q':query, 'from':from_, 'to':to, 'appid':appid, 'salt':salt, 'sign':sign})
-
-    r = requests.get(url)
-    r.encoding = 'utf-8'
-    result = r.json()
-    if 'error_code' in result:
-        # raise TransLationError
-        return None
-
-    if 'trans_result' in result:
-        return result['trans_result'][0].get('dst', None)
-
-    return None
+    def replace(self, string):
+        '''
+        将空格转换成 -
+        '''
+        return string.replace(' ', '-')
+#
+# def main():
+#     argv = sys.argv
+#
+#     if len(argv) > 1:
+#         make(argv)
+#
+# def make(argv):
+#     if len(argv) > 1:
+#         if argv[1] == 'create':
+#             return create(argv)
+#         else: return None
+#
+# def create(argv):
+#     '''
+#     创建指定的文章
+#     '''
+#     if len(argv) > 2:
+#         filename = (time.strftime(FILENAME, time.localtime())).format(title=argv[2])
+#         content = argv[2] + '\n' + (len(argv[2])+2)*'=' + \
+#                 '\n:date: '+ time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + \
+#                 '\n:slug: ' + replace(translation(argv[2])) + \
+#                 '\n:modified: ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + \
+#                 '\n:author: ' + AUTHOR
+#
+#         ext = '.rst' if type_ == 'resTructured' else '.md'
+#         with open(PATH+'/'+filename+ext, 'w') as fp:
+#             fp.write(content)
+#
+# def replace(string):
+#     '''
+#     将空格转换成 -
+#     '''
+#     return string.replace(' ', '-')
+#
+# def translation(query, from_ = 'auto', to = 'en'):
+#     '''
+#     请求百度翻译 api 获取翻译结果
+#     '''
+#     appid = '20161217000034172'
+#     secretKey = '07Qh2zEwKIx3kGwer1Uz'
+#     salt = random.randint(32768, 65536)
+#     sign = appid+query+str(salt)+secretKey
+#     m1 = hashlib.md5(sign.encode('utf8'))
+#     sign = m1.hexdigest()
+#     url ='http://api.fanyi.baidu.com/api/trans/vip/translate?'
+#     url = url + urllib.parse.urlencode({'q':query, 'from':from_, 'to':to, 'appid':appid, 'salt':salt, 'sign':sign})
+#
+#     r = requests.get(url)
+#     r.encoding = 'utf-8'
+#     result = r.json()
+#     if 'error_code' in result:
+#         # raise TransLationError
+#         return None
+#
+#     if 'trans_result' in result:
+#         return result['trans_result'][0].get('dst', None)
+#
+#     return None
 
 if __name__ == '__main__':
-    # main()
     autotool = Autotool(sys.argv)
     # autotool.read_pelicanconfig()
-    print(autotool.data)
+    # print(autotool.data)
     autotool.get_path()
     autotool.run()
     # print(autotool.data)
