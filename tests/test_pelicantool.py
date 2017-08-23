88bbf06c1523facca87d9b4915884ee4ceb056a9
xiaojieluo <xiaojieluoff@gmail.com>
2017-08-24 07:47:04 +0800

    delete comments

--- tests/test_pelicantool.py
+++ tests/test_pelicantool.py
@@ -21,10 +21,7 @@ class TestPelicantool(object):
 
     def test_article_create(self):
         self.article.title = '测试标题'
-        # self.article.create()
-        # filename = os.path.join('tests/content', self.article.filename)
         self.article.create(expath='tests/content')
-        # print(filename)
 
         assert os.path.isfile(os.path.join('tests/content',self.article.filename)) is True
 
