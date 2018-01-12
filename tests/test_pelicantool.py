58160ea7abfc3db61c08867889b2fdf92c68e34e
xiaojieluo <xiaojieluoff@gmail.com>
2018-01-12 15:01:08 +0800

    modify unit test

--- tests/test_pelicantool.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from pelicantool import Article
-from pelicantool import pelicantool
-import os
-
-def test_pelicantool_create_markdown_article():
-    print(pelicantool.main())
-    # pass
